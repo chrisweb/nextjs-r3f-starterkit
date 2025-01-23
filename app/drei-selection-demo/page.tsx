af2b4e1407113d1aa5336225a9c1d296e952983b
chrisweb <chris963@gmail.com>
2025-01-23 14:35:23 +0100

    finish drei selection demo

---
+++ app/drei-selection-demo/page.tsx
@@ -0,0 +1,10 @@
+import DreiSelectionDemo from "@/components/drei/selection/Demo"
+
+export default function DreiSelectionDemoPage() {
+    return (
+        <>
+            <h1>Drei &quot;Selection&quot; Demo</h1>
+            <DreiSelectionDemo />
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ export default function Home() {
     return (
         <>
             <h1>Examples</h1>
-            <Link href="/basic-demo">Basic demo</Link>
-            <Link href="/postprocessing-demo">Postprocessing demo</Link>
-            <Link href="/postprocessing-demo">Drei &quot;Selection&quot; demo</Link>
+            <ul>
+                <li><Link href="/basic-demo">Basic demo</Link></li>
+                <li><Link href="/postprocessing-demo">Postprocessing demo</Link></li>
+                <li><Link href="/drei-selection-demo">Drei &quot;Selection&quot; demo</Link></li>
+            </ul>
         </>
     )
 }
