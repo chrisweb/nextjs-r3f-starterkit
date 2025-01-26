0c3cf38626383332d44d05fc2130ce7c0662aef3
chrisweb <chris963@gmail.com>
2025-01-26 19:39:40 +0100

    need to investigate, xr seems bugged when using r3f 9 and react 19

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,11 @@ export default function Home() {
         <>
             <h1>Examples</h1>
             <ul>
-                <li><Link href="/basic-demo">Basic demo</Link> (works)</li>
+                <li><Link href="/basic-demo">Basic R3F demo</Link> (works)</li>
                 <li><Link href="/postprocessing-demo">Postprocessing demo</Link> (works)</li>
                 <li><Link href="/drei-selection-demo">Drei &quot;Selection&quot; demo</Link> (fails)</li>
                 <li><Link href="/leva-demo">Leva demo</Link> (works)</li>
+                <li><Link href="/xr-demo">XR demo</Link> (fails)</li>
             </ul>
         </>
     )
---
+++ app/xr-demo/page.tsx
@@ -0,0 +1,10 @@
+import XRDemo from "@/components/xr/Demo"
+
+export default function XRDemoPage() {
+    return (
+        <>
+            <h1>XR Demo</h1>
+            <XRDemo />
+        </>
+    )
+}
